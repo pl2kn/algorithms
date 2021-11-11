622db741a48925df9678260c2a7b42e3c8a26383
plknk <pavel.konkin.dev@gmail.com>
2021-11-11 12:21:36 +0300

    Sort. Insertion sort refactoring

--- src/main/java/com/pl2kn/algorithms/sort/InsertionSort.java
+++ src/main/java/com/pl2kn/algorithms/sort/InsertionSort.java
@@ -8,12 +8,8 @@ public class InsertionSort extends Sort {
   @Override
   public <T extends Comparable<T>> void sort(T[] array) {
     for (int i = 0; i < array.length; i++) {
-      for (int j = i; j > 0; j--) {
-        if (array[j].compareTo(array[j - 1]) < 0) {
-          swap(array, j, j - 1);
-        } else {
-          break;
-        }
+      for (int j = i; j > 0 && array[j].compareTo(array[j - 1]) < 0; j--) {
+        swap(array, j, j - 1);
       }
     }
   }
