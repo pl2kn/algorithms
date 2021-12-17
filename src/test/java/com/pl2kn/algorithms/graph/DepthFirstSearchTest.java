5c61f807bdbf0df33e2ead6bc8d760b9624a3b98
plknk <pavel.konkin.dev@gmail.com>
2021-12-17 10:55:23 +0300

    Graphs. BFS.

--- src/test/java/com/pl2kn/algorithms/graph/DepthFirstSearchTest.java
+++ src/test/java/com/pl2kn/algorithms/graph/DepthFirstSearchTest.java
@@ -9,62 +9,37 @@ public class DepthFirstSearchTest {
 
   @Test
   public void Has_Path_Test() {
-    Graph graph = new Graph(13);
-    graph.addEdge(0, 1);
-    graph.addEdge(0, 2);
-    graph.addEdge(0, 6);
-    graph.addEdge(0, 5);
-    graph.addEdge(6, 4);
-    graph.addEdge(4, 5);
-    graph.addEdge(4, 3);
-    graph.addEdge(3, 5);
-    graph.addEdge(7, 8);
-    graph.addEdge(9, 10);
-    graph.addEdge(9, 11);
-    graph.addEdge(9, 12);
-    graph.addEdge(11, 12);
+    DepthFirstSearch dfs = new DepthFirstSearch(createGraph(), 0);
+    assertTrue(dfs.hasPathTo(5));
+    assertTrue(dfs.hasPathTo(3));
+    assertTrue(dfs.hasPathTo(4));
+    assertTrue(dfs.hasPathTo(6));
+    assertTrue(dfs.hasPathTo(1));
+    assertTrue(dfs.hasPathTo(2));
 
-    DepthFirstSearch dps = new DepthFirstSearch(graph, 0);
-    assertTrue(dps.hasPathTo(5));
-    assertTrue(dps.hasPathTo(3));
-    assertTrue(dps.hasPathTo(4));
-    assertTrue(dps.hasPathTo(6));
-    assertTrue(dps.hasPathTo(1));
-    assertTrue(dps.hasPathTo(2));
-
-    assertFalse(dps.hasPathTo(7));
-    assertFalse(dps.hasPathTo(8));
-    assertFalse(dps.hasPathTo(9));
-    assertFalse(dps.hasPathTo(12));
+    assertFalse(dfs.hasPathTo(7));
+    assertFalse(dfs.hasPathTo(8));
+    assertFalse(dfs.hasPathTo(9));
+    assertFalse(dfs.hasPathTo(12));
   }
 
   @Test
   public void Path_To_Test() {
-    Graph graph = new Graph(13);
-    graph.addEdge(0, 1);
-    graph.addEdge(0, 2);
-    graph.addEdge(0, 6);
-    graph.addEdge(0, 5);
-    graph.addEdge(6, 4);
-    graph.addEdge(4, 5);
-    graph.addEdge(4, 3);
-    graph.addEdge(3, 5);
-    graph.addEdge(7, 8);
-    graph.addEdge(9, 10);
-    graph.addEdge(9, 11);
-    graph.addEdge(9, 12);
-    graph.addEdge(11, 12);
-
-    DepthFirstSearch dps = new DepthFirstSearch(graph, 0);
+    DepthFirstSearch dfs = new DepthFirstSearch(createGraph(), 0);
     Stack<Integer> expectedPath = new Stack<>();
     expectedPath.add(4);
     expectedPath.add(6);
     expectedPath.add(0);
-    assertEquals(expectedPath, dps.pathTo(4));
+    assertEquals(expectedPath, dfs.pathTo(4));
   }
 
   @Test
   public void No_Path_To_Test() {
+    DepthFirstSearch dfs = new DepthFirstSearch(createGraph(), 0);
+    assertNull(dfs.pathTo(12));
+  }
+
+  private Graph createGraph() {
     Graph graph = new Graph(13);
     graph.addEdge(0, 1);
     graph.addEdge(0, 2);
@@ -80,7 +55,6 @@ public class DepthFirstSearchTest {
     graph.addEdge(9, 12);
     graph.addEdge(11, 12);
 
-    DepthFirstSearch dps = new DepthFirstSearch(graph, 0);
-    assertNull(dps.pathTo(12));
+    return graph;
   }
 }
